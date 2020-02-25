1438df684b388e32f4b91b312c0db4296b62980f
fonsecadh <UO258318@uniovi.es>
2020-02-25 19:35:15 +0100

    SDI-SpringBoot-Sesión-4.3-Control de acceso por roles.

--- src/main/java/com/uniovi/entities/Mark.java
+++ src/main/java/com/uniovi/entities/Mark.java
@@ -14,7 +14,8 @@ public class Mark {
 	private Long id;
 	private String description;
 	private Double score;
-	
+	private Boolean resend = false;
+
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
@@ -67,6 +68,14 @@ public class Mark {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public Boolean getResend() {
+		return resend;
+	}
+
+	public void setResend(Boolean resend) {
+		this.resend = resend;
 	}
 
 	@Override
